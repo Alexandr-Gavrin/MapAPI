365ca2bf98a54acab2957142eeb5b98b7a985807
Александр <nuke.dust@yandex.ru>
2020-02-20 21:29:08 +0300

    A big task for the Maps API. Part 5

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class MyWidget(QMainWindow):
         self.spn = [0.002, 0.002]
         self.L = 'map'
         self.format_of_map = "png"
+        self.pt = None
         self.ll = [37.530887, 55.703118]
         self.map_api_server = "http://static-maps.yandex.ru/1.x/"
         self.initui()
@@ -30,13 +31,34 @@ class MyWidget(QMainWindow):
         self.btn_sat.setStyleSheet("QPushButton {background: green;}")
         self.btn_sat_skl.clicked.connect(self.change_to_sat_skl)
         self.btn_sat_skl.setStyleSheet("QPushButton {background: green;}")
-        self.btn_reload.clicked.connect(self.reload_map)
-        self.btn_reload.setStyleSheet("QPushButton {background: #aa00ff;}")
+        self.btn_search.clicked.connect(self.search)
+
+    def search(self):
+        try:
+            toponym_to_find = self.line_to_search.text()
+            geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+            geocoder_params = {
+                "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+                "geocode": toponym_to_find,
+                "format": "json"}
+            response = requests.get(geocoder_api_server, params=geocoder_params)
+            json_response = response.json()
+            toponym = json_response["response"]["GeoObjectCollection"]["featureMember"][0][
+                "GeoObject"]
+            toponym_coodrinates = toponym["Point"]["pos"]
+            self.ll = toponym_coodrinates.split(" ")
+            self.pt = toponym_coodrinates.split(" ")
+            self.run()
+        except Exception:
+            pass
 
     def change_to_map(self):
         self.L = "map"
         self.format_of_map = "png"
         self.run()
+
+    def size(self, x1, y1, x2, y2):
+        return [str(abs(float(x1) - float(x2))), str(abs(float(y1) - float(y2)))]
 
     def change_to_sat(self):
         self.L = "sat"
@@ -48,30 +70,36 @@ class MyWidget(QMainWindow):
         self.format_of_map = "jpg"
         self.run()
 
-    def reload_map(self):
-        self.run()
-
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageUp:
-            self.spn = [min(10.0, self.spn[0] + 0.001), min(10.0, self.spn[1] + 0.001)]
+            self.spn = [min(10.0, self.spn[0] + 0.01), min(10.0, self.spn[1] + 0.01)]
         elif event.key() == Qt.Key_PageDown:
-            self.spn = [max(0, self.spn[0] - 0.001), max(0, self.spn[1] - 0.001)]
+            self.spn = [max(0, self.spn[0] - 0.01), max(0, self.spn[1] - 0.01)]
         elif event.key() == Qt.Key_Up:
-            self.ll = [self.ll[0], self.ll[1] + 0.0001]
+            self.ll = [self.ll[0], self.ll[1] + 0.001]
         elif event.key() == Qt.Key_Down:
-            self.ll = [self.ll[0], self.ll[1] - 0.0001]
+            self.ll = [self.ll[0], self.ll[1] - 0.001]
         elif event.key() == Qt.Key_Left:
-            self.ll = [self.ll[0] - 0.0001, self.ll[1]]
+            self.ll = [self.ll[0] - 0.001, self.ll[1]]
         elif event.key() == Qt.Key_Right:
-            self.ll = [self.ll[0] + 0.0001, self.ll[1]]
+            self.ll = [self.ll[0] + 0.001, self.ll[1]]
         self.run()
 
     def run(self):
-        map_params = {
-            "ll": f"{self.ll[0]},{self.ll[1]}",
-            "spn": f"{self.spn[0]},{str(self.spn[1])}",
-            "l": self.L
-        }
+        if self.pt:
+            map_params = {
+                "ll": f"{self.ll[0]},{self.ll[1]}",
+                "spn": f"{self.spn[0]},{str(self.spn[1])}",
+                "l": self.L,
+                "pt": "{},pm2dgl".format(','.join(self.ll))
+            }
+        else:
+            map_params = {
+                "ll": f"{self.ll[0]},{self.ll[1]}",
+                "spn": f"{self.spn[0]},{str(self.spn[1])}",
+                "l": self.L
+            }
+
         response = requests.get(self.map_api_server, params=map_params)
         self.map_file = f"map.{self.format_of_map}"
         with open(self.map_file, "wb") as file:
